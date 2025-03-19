5347bf2af7ea232dc452093de38479e34cfce112
Ken Barber <ken@bob.sh>
2025-03-20 04:39:29 +1000

    feat: add new entity components and bundles for Warrior, Jungleman, Mine, Quarry, and Tree2

--- src/entities.rs
+++ src/entities.rs
@@ -4,34 +4,64 @@ use bevy_ecs_ldtk::prelude::*;
 
 pub struct EntitiesPlugin;
 
+#[derive(Default, Component)]
+struct Warrior;
+
 #[derive(Default, Bundle, LdtkEntity)]
 struct WarriorBundle {
+    warrior: Warrior,
     #[sprite_sheet]
     sprite_sheet: Sprite,
+    #[grid_coords]
+    grid_coords: GridCoords,
 }
+
+#[derive(Default, Component)]
+struct Jungleman;
 
 #[derive(Default, Bundle, LdtkEntity)]
 struct JunglemanBundle {
+    jungleman: Jungleman,
     #[sprite_sheet]
     sprite_sheet: Sprite,
+    #[grid_coords]
+    grid_coords: GridCoords,
 }
+
+#[derive(Default, Component)]
+struct Mine;
 
 #[derive(Default, Bundle, LdtkEntity)]
 struct MineBundle {
+    mine: Mine,
     #[sprite_sheet]
     sprite_sheet: Sprite,
+    #[grid_coords]
+    grid_coords: GridCoords,
 }
+
+#[derive(Default, Component)]
+struct Quarry;
 
 #[derive(Default, Bundle, LdtkEntity)]
 struct QuarryBundle {
+    quarry: Quarry,
     #[sprite_sheet]
     sprite_sheet: Sprite,
+    #[grid_coords]
+    grid_coords: GridCoords,
 }
+
+#[derive(Default, Component)]
+struct Tree2;
 
 #[derive(Default, Bundle, LdtkEntity)]
 struct Tree2Bundle {
+    tree2: Tree2,
     #[sprite_sheet]
     sprite_sheet: Sprite,
+    #[grid_coords]
+    grid_coords: GridCoords,
 }
 
 impl Plugin for EntitiesPlugin {
