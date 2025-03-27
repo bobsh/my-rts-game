78f8dcc9f3e372fafb9b7ce992487f80f03bd86e
kbarber <ken@bob.sh>
2025-03-27 12:57:31 +0000

    Deploying to gh-pages from @ bobsh/my-rts-game@3f7a4426cee506e3b38373a38eec16b0ee34882a 🚀

--- main/my-rts-game.d.ts
+++ main/my-rts-game.d.ts
@@ -13,13 +13,13 @@ export interface InitOutput {
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_6: WebAssembly.Table;
-  readonly closure2513_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure2582_externref_shim: (a: number, b: number, c: any) => void;
   readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb247e00d32032336: (a: number, b: number, c: number) => void;
-  readonly closure14509_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure14578_externref_shim: (a: number, b: number, c: any) => void;
   readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3bfec4b7090c4e97: (a: number, b: number) => void;
-  readonly closure14514_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly closure14583_externref_shim: (a: number, b: number, c: any, d: any) => void;
   readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5a2250d40a6cfd25: (a: number, b: number) => void;
-  readonly closure111466_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure111534_externref_shim: (a: number, b: number, c: any) => void;
   readonly __wbindgen_start: () => void;
 }
 
