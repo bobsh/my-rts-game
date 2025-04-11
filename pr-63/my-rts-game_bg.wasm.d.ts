358bcbaa7ad98323f063a184a46c93df962b7040
kbarber <kbarber@users.noreply.github.com>
2025-04-11 16:43:22 +0000

    Deploying to gh-pages from @ bobsh/my-rts-game@21ae38adcf653f69309f5a483cd25b8bc3de29fe 🚀

---
+++ pr-63/my-rts-game_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0fae3e7efd036592: (a: number, b: number) => void;
+export const closure4073_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hcf228e76132af99b: (a: number, b: number, c: number) => void;
+export const closure16513_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h4263dde957abd64e: (a: number, b: number) => void;
+export const closure16515_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h238a7212ee9c9f00: (a: number, b: number) => void;
+export const closure114126_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
