185ff62b117b52964a1e8ea8725e5fb2c97992d2
Ken Barber <ken@bob.sh>
2025-04-14 01:02:36 +1000

    Fix volume, add new character sprites, new chest, and some new terrains

--- src/systems/audio.rs
+++ src/systems/audio.rs
@@ -12,5 +12,5 @@ impl Plugin for AudioSystemPlugin {
 
 fn play_background_music(asset_server: Res<AssetServer>, audio: Res<Audio>) {
     let music = asset_server.load("music/first_steps.mp3");
-    audio.play(music).looped();
+    audio.play(music).looped().with_volume(0.1);
 }
