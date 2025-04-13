1fa5210a9f57a3e63e325d1d754898c09ccbdb1e
Ken Barber <ken@bob.sh>
2025-04-14 02:20:03 +1000

    Fix volume and add new assets including character sprites and terrains (#67)
    * Fix volume, add new character sprites, new chest, and some new terrains
    
    * fix
    
    * Add walls1.png asset to the project
    
    * Remove unused asset files from the project

--- src/systems/audio.rs
+++ src/systems/audio.rs
@@ -5,12 +5,12 @@ pub struct AudioSystemPlugin;
 
 impl Plugin for AudioSystemPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(AudioPlugin) // Add the AudioPlugin to initialize the audio system
+        app.add_plugins(AudioPlugin)
             .add_systems(Startup, play_background_music);
     }
 }
 
 fn play_background_music(asset_server: Res<AssetServer>, audio: Res<Audio>) {
     let music = asset_server.load("music/first_steps.mp3");
-    audio.play(music).looped();
+    audio.play(music).looped().with_volume(0.1);
 }
